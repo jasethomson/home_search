1b9e6ba50954f6130dd27b154322bafcd6b1aa52
Jase Thomson <jasethomson31@gmail.com>
2023-08-23 10:11:52 -0700

    remove useless conditional

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,14 @@ require('dotenv').config();
 (async () => {
     try {
         const htmlStr = await requestHtml(process.env.MOUNT_TABOR_REDFIN_URL);
-        if (htmlStr) {
-            const homeLinks = getHomeLinks(htmlStr);
-            const homePromises: Promise<Home | null>[] = [];
-            homeLinks.forEach((link, i) => {
-                const homeLink = `${process.env.ROOT_REDFIN_URL}${link}`;
-                homePromises.push(processHome(homeLink));
-            });
-            const homes = await Promise.all(homePromises);
-            console.log(homes);
-        }
+        const homeLinks = getHomeLinks(htmlStr);
+        const homePromises: Promise<Home | null>[] = [];
+        homeLinks.forEach((link, i) => {
+            const homeLink = `${process.env.ROOT_REDFIN_URL}${link}`;
+            homePromises.push(processHome(homeLink));
+        });
+        const homes = await Promise.all(homePromises);
+        console.log(homes);
     } catch (err) {
         console.error(`Error thrown while running app: ${err}`);
     }
